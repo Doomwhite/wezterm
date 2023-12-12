a1b940939cb735b897820a5a2fbbf5a892644bb1
Adinelson Brühmüller <adinelson.bruhmuller@movtech.com.br>
2023-12-12 16:16:14 -0300

    fix: removes unwanted characters.

--- wezterm.lua
+++ wezterm.lua
@@ -1,6 +1,6 @@
 return require "utils.functions".config.merge_all(
 	require("config.misc"),
-gv	{ launch_menu = require(require("../utils/directories").directories.getLaunchMenuDir()) },
+	{ launch_menu = require(require("../utils/directories").directories.getLaunchMenuDir()) },
 	{ keys = require("config.keys") },
 	{ key_tables = require("config.key_tables") }
 )
