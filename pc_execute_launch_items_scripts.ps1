8bd4a6be0b28399e65d81db804bae9790f6b9395
Adinelson Brühmüller <adinelson.bruhmuller@movtech.com.br>
2023-12-22 01:35:15 -0300

    feat(launch menu items): adds the books section.

--- pc_execute_launch_items_scripts.ps1
+++ pc_execute_launch_items_scripts.ps1
@@ -6,7 +6,7 @@ if (-not $env:WEZTERM_HOME) {
 }
 
 $launchItemsDir = "\Documents\Projects"
-$launchMenuDir = Join-Path $env:WEZTERM_HOME ".config\wezterm\config\launch_menu_work.lua"
+$launchMenuDir = Join-Path $env:WEZTERM_HOME ".config\wezterm\config\launch_menu_pc.lua"
 
 Set-Location "launch_item_scripts"
 & .\lua.bat wezterm_include_launch_items.lua --key=zig --directory="$launchItemsDir\Zig" --launch_menu_directory=$launchMenuDir
@@ -15,4 +15,5 @@ Set-Location "launch_item_scripts"
 & .\lua.bat wezterm_include_launch_items.lua --key=nix --directory="$launchItemsDir\Nix" --launch_menu_directory=$launchMenuDir
 & .\lua.bat wezterm_include_launch_items.lua --key=.config --directory="\.config" --launch_menu_directory=$launchMenuDir
 & .\lua.bat wezterm_include_launch_items.lua --key=alura --directory="$launchItemsDir\Alura" --launch_menu_directory=$launchMenuDir
+& .\lua.bat wezterm_include_launch_items.lua --key=books --directory="$launchItemsDir\Books" --launch_menu_directory=$launchMenuDir
 & cd ..
--- work_execute_launch_items_scripts.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd "launch_item_scripts"
-call lua.bat wezterm_include_launch_items.lua --key=zig --directory="\\Documents\\Projects\\Zig\\" --launch_menu_directory="C:/Users/Cliente/.config/wezterm/config/launch_menu_work.lua"
-call lua.bat wezterm_include_launch_items.lua --key=git --directory="\\Documents\\Projects\\Git\\" --launch_menu_directory="C:/Users/Cliente/.config/wezterm/config/launch_menu_work.lua"
-call lua.bat wezterm_include_launch_items.lua --key=rust --directory="\\Documents\\Projects\\Rust\\" --launch_menu_directory="C:/Users/Cliente/.config/wezterm/config/launch_menu_work.lua"
-call lua.bat wezterm_include_launch_items.lua --key=nix --directory="\\Documents\\Projects\\Nix\\" --launch_menu_directory="C:/Users/Cliente/.config/wezterm/config/launch_menu_work.lua"
-call lua.bat wezterm_include_launch_items.lua --key=.config --directory="\\.config\\" --launch_menu_directory="C:/Users/Cliente/.config/wezterm/config/launch_menu_work.lua"
-call lua.bat wezterm_include_launch_items.lua --key=alura --directory="\\Documents\\Projects\\Alura\\" --launch_menu_directory="C:/Users/Cliente/.config/wezterm/config/launch_menu_work.lua"
